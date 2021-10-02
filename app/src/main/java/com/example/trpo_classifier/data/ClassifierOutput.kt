a4787a51f66645d4cbe2c7229a3aeaae77ade04d
bzzbzzbee <chikipony666@gmail.com>
2021-10-03 00:58:43 +0700

    Unit tests

--- app/src/main/java/com/example/trpo_classifier/data/ClassifierOutput.kt
+++ app/src/main/java/com/example/trpo_classifier/data/ClassifierOutput.kt
@@ -2,7 +2,10 @@ package com.example.trpo_classifier.data
 
 data class ClassifierOutput(val name: String, val confidence: Float) {
     override fun toString(): String {
-        val percent = (confidence * 100).toInt()
+        val percent = confidence.toPercent()
         return "$name: $percent%"
     }
 }
+
+fun Float.toPercent(): Int = (this * 100).toInt()
+
--- app/src/test/java/com/example/trpo_classifier/ExampleUnitTest.kt
+++ app/src/test/java/com/example/trpo_classifier/ExampleUnitTest.kt
@@ -1,17 +1,40 @@
 package com.example.trpo_classifier
 
+import com.example.trpo_classifier.data.ClassifierOutput
+import com.example.trpo_classifier.data.toPercent
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+//TODO test cases:
+//Data class toString+
+//Float toPercent+
+//Classifier img test
+//Classifier confidence test
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun floatToPercentIsCorrect() {
+        val test = 0.15f.toPercent()
+        val testInt = 15
+        assertEquals(testInt, test)
+    }
+
+    @Test
+    fun dataClassToStringIsCorrect() {
+        val testName = "test"
+        val testConf = 0.15f
+        val test = ClassifierOutput(testName, testConf)
+        val testText = "$testName ${testConf.toPercent()}%"
+        assertEquals(testText, test.toString())
     }
 }
